51226d4aefd1255762a40f5f0cec39dc7a0b775b
Julie Pierre <julie.pierre3@gmail.com>
2018-01-05 12:49:08 +0100

    trying to add animation to scroll" "

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -5,19 +5,21 @@ document.addEventListener("DOMContentLoaded", () => {
   document.getElementById(currentLink).classList.add("active");
 
   // Smooth scroll
-  // if (document.getElementById('clients-list')) {
-  //   const height = window.innerHeight;
-  //   const clientsList = document.getElementById('clients-list').dataset.info;
-  //   const info = JSON.parse(clientsList);
-  //   window.addEventListener('scroll', function(e) {
-  //     info.forEach((client) => {
-  //       target = document.getElementById(client);
-  //       if ((target.getBoundingClientRect().top < (height - 100)) && (target.getBoundingClientRect().top > (height - 300))) {
-  //         target.scrollIntoView();
-  //       };
-  //     });
-  //   });
-  // };
+  if (document.getElementById('clients-list')) {
+    const height = window.innerHeight;
+    const clientsList = document.getElementById('clients-list').dataset.info;
+    const info = JSON.parse(clientsList);
+    window.addEventListener('scroll', function(e) {
+      test = document.getElementById('otium-capital').getBoundingClientRect().top;
+      console.log(test);
+      info.forEach((client) => {
+        target = document.getElementById(client);
+        if ((target.getBoundingClientRect().top < (height - 400)) && (target.getBoundingClientRect().top > 0)) {
+          target.scrollIntoView({behavior: 'smooth', block: 'start'});
+        };
+      });
+    });
+  };
 
 
 });
