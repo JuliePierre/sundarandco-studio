9de82ca2861980ca53518b3df83370e07b348c3c
Julie Pierre <julie.pierre3@gmail.com>
2018-01-05 10:54:32 +0100

    adding js files

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,21 +1,23 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   // Add here your addEventListener code
-//   let currentPage = document.querySelectorAll(".page-index");
-//   let currentLink = currentPage[0].dataset.page;
-//   document.getElementById(currentLink).classList.add("active");
+document.addEventListener("DOMContentLoaded", () => {
+  // Add here your addEventListener code
+  let currentPage = document.querySelectorAll(".page-index");
+  let currentLink = currentPage[0].dataset.page;
+  document.getElementById(currentLink).classList.add("active");
 
-//   // Smooth scroll
-//   if (document.getElementById('clients-list')) {
-//     const height = window.innerHeight;
-//     const clientsList = document.getElementById('clients-list').dataset.info;
-//     const info = JSON.parse(clientsList);
-//     window.addEventListener('scroll', function(e) {
-//       info.forEach((client) => {
-//         target = document.getElementById(client);
-//         if ((target.getBoundingClientRect().top < (height - 100)) && (target.getBoundingClientRect().top > (height - 300))) {
-//           target.scrollIntoView();
-//         };
-//       });
-//     });
-//   };
-// });
+  // Smooth scroll
+  // if (document.getElementById('clients-list')) {
+  //   const height = window.innerHeight;
+  //   const clientsList = document.getElementById('clients-list').dataset.info;
+  //   const info = JSON.parse(clientsList);
+  //   window.addEventListener('scroll', function(e) {
+  //     info.forEach((client) => {
+  //       target = document.getElementById(client);
+  //       if ((target.getBoundingClientRect().top < (height - 100)) && (target.getBoundingClientRect().top > (height - 300))) {
+  //         target.scrollIntoView();
+  //       };
+  //     });
+  //   });
+  // };
+
+
+});
