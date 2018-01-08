444b19b0bfc33f3a3c0a85f79307e5a40a0c2271
Julie Pierre <julie.pierre3@gmail.com>
2018-01-08 13:23:55 +0100

    ajout de l'italique sur les liens du menu mobile

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -2,24 +2,26 @@ document.addEventListener("DOMContentLoaded", () => {
   // Add here your addEventListener code
   let currentPage = document.querySelectorAll(".page-index");
   let currentLink = currentPage[0].dataset.page;
+  let mobileCurrentLink = "mobile-" + currentPage[0].dataset.page;
   document.getElementById(currentLink).classList.add("active");
+  document.getElementById(mobileCurrentLink).classList.add("active");
 
   // Smooth scroll
-  if (document.getElementById('clients-list')) {
-    const height = window.innerHeight;
-    const clientsList = document.getElementById('clients-list').dataset.info;
-    const info = JSON.parse(clientsList);
-    window.addEventListener('scroll', function(e) {
-      test = document.getElementById('otium-capital').getBoundingClientRect().top;
-      console.log(test);
-      info.forEach((client) => {
-        target = document.getElementById(client);
-        if ((target.getBoundingClientRect().top < (height - 400)) && (target.getBoundingClientRect().top > 0)) {
-          target.scrollIntoView({behavior: 'smooth', block: 'start'});
-        };
-      });
-    });
-  };
+  // if (document.getElementById('clients-list')) {
+  //   const height = window.innerHeight;
+  //   const clientsList = document.getElementById('clients-list').dataset.info;
+  //   const info = JSON.parse(clientsList);
+  //   window.addEventListener('scroll', function(e) {
+  //     test = document.getElementById('otium-capital').getBoundingClientRect().top;
+  //     console.log(test);
+  //     info.forEach((client) => {
+  //       target = document.getElementById(client);
+  //       if ((target.getBoundingClientRect().top < (height - 400)) && (target.getBoundingClientRect().top > 0)) {
+  //         target.scrollIntoView({behavior: 'smooth', block: 'start'});
+  //       };
+  //     });
+  //   });
+  // };
 
   // Définition des fonctions
   function showNavbar() {
